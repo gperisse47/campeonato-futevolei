29452bc05076bb8ace047155687f1a3bec8b9f70
gperisse47 <gabrielperisse47@gmail.com>
2025-08-13 18:58:25 -0300

    Update scheduler.ts
    revert

--- src/lib/scheduler.ts
+++ src/lib/scheduler.ts
@@ -287,30 +287,29 @@ export function scheduleMatches(matchesInput: MatchRow[], parameters: Record<str
 
     // 4. Aloca as melhores partidas nas melhores quadras
     for (let i = 0; i < Math.min(sortedCourts.length, candidateMatches.length); i++) {
-        const court = sortedCourts[i];
-        const match = candidateMatches[i];
-        
+      const court = sortedCourts[i];
+      const match = candidateMatches[i];
+
+
         match.time = formatDate(currentTime, "HH:mm");
         match.court = court.name;
         court.nextAvailable = addMinutes(currentTime, matchDuration);
-        
+
         for (const p of match.players) {
             playerAvailability[p] = addMinutes(currentTime, matchDuration);
-            
             if (!matchHistory[p]) matchHistory[p] = [];
             matchHistory[p].push(currentTime);
-            
             usedPlayers.add(p);
         }
-        
         unscheduled.delete(match.id);
     }
     
     if (isEqual(currentTime, loopStartTime)) {
-        currentTime = addMinutes(currentTime, matchDuration);
+      currentTime = addMinutes(currentTime, matchDuration);
     } else {
-        currentTime = loopStartTime;
+      currentTime = loopStartTime;
     }
+  }
 
   if (unscheduled.size > 0 && addMinutes(currentTime, matchDuration) > END_OF_DAY) {
       for(const id of unscheduled){
